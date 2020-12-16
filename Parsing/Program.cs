c8ab07125f53382ea54bb08fc0dd9a58877c6678
Dara Oladapo <doladapo@qa.com>
2020-12-16 09:46:42 +0000

    Add project files.

---
+++ Parsing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intString = "55";
+            string doubleString = "55.5";
+
+            int myInt = int.Parse(intString);
+            Console.WriteLine(myInt);
+
+            double mydouble = double.Parse(doubleString);
+            Console.WriteLine(mydouble);
+        }
+    }
+}
